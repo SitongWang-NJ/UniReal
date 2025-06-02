7aaf6e719e5fe88a02768372b6a1315be7f3a4c9
Sorry <553414537@qq.com>
2025-06-02 10:29:40 +0800

    更新home导航设计

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,40 +1,21 @@
 // pages/home/home.js
 Page({
-  data: {
-    currentTimeSlot: null,
-    timeSlots: [
-      { id: 1, start: '14:40', end: '15:00' },
-      { id: 2, start: '15:00', end: '15:20' },
-      { id: 3, start: '15:20', end: '15:40' },
-    ]
+  data:{
+    latitude:"23.013833",
+    longitude:"113.404188",
+    latitude_click:"",
+    longtitude_click:""
+  },
+  /**
+   * 地点地图事件
+   * @param {*} e
+   */
+  mapBindtap(e) {
+    console.log(e.detail);
+    this.setData(
+      longtitude_click=e.longitude,
+      latitude_click=e.latitude
+    )
   },
 
-  onLoad(options) {
-    // 初始化
-  },
-
-  selectTimeSlot(e) {
-    const selectedId = e.currentTarget.dataset.id;
-    this.setData({
-      currentTimeSlot: selectedId
-    });
-    this.updateRecommendations(selectedId);
-  },
-
-  updateRecommendations(timeSlotId) {
-    console.log('更新推荐商家，时间段ID:', timeSlotId);
-    // 可根据时间段请求新数据
-  },
-
-  goToSearch() {
-    wx.navigateTo({
-      url: '/pages/restaurant_search/restaurant_search',
-    });
-  },
-
-  goToWorkDetail() {
-    wx.navigateTo({
-      url: '/pages/work_status_detail/work_status_detail'
-    });    
-  }
 });
